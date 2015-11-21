7eeac7ff94b03d9e9dc6156b0127a78ba979fe2e
Andrew Wei <andrew@variante.io>
2015-11-20 19:15:43 -0800

    Fix random()

--- src/math/random.js
+++ src/math/random.js
@@ -14,16 +14,14 @@ let assertType = require('../helpers/assertType');
  * Generates a random real number within the given range, starting from the
  * minimum (inclusive) to the maximum (exclusive).
  *
- * @param {number}  value - Value of interest.
- * @param {number}  min   - Min value (inclusive).
- * @param {number}  max   - Max value (exclusive).
+ * @param {number} min - Min value (inclusive).
+ * @param {number} max - Max value (exclusive).
  *
  * @return {number} Random number within the given range.
  *
  * @alias module:vars~math.random
  */
-function random(value, min, max) {
-  assertType(value, 'number', false, 'Invalid value specified');
+function random(min, max) {
   assertType(min, 'number', false, 'Parameter \'min\' must be a number');
   assertType(max, 'number', false, 'Parameter \'max\' must be a number');
 
